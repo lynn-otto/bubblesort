77d4dba11bb71edce90b41eb5f0d19a909acd2ca
Philipp Otto <philotto95@gmail.com>
2022-03-23 10:57:25 +0100

    Write bubble_sort! and bubble_sort

--- bubble.rb
+++ bubble.rb
@@ -1,28 +1,29 @@
-def switch?(left_value, right_value)
-  return left_value > right_value
+def switch?(array, index)
+  return array[index] > array[index+1]
 end
 
-def switch_places!(array, first_index, second_index)
-  first_element = array[first_index]
-  second_element = array[second_index]
-  array[first_index] = second_element
-  array[second_index] = first_element
+def switch_places!(array, index)
+  first_element = array[index]
+  second_element = array[index+1]
+  array[index] = second_element
+  array[index+1] = first_element
 end
 
-def bubble_sort(array)
+def bubble_sort!(array)
   changes_happened = true
   while changes_happened
     changes_happened = false
     (array.length-1).times do |i|
-      if switch?(array[i],array[i+1])
+      if switch?(array,i)
         changes_happened = true
-        switch_places!(array,i,i+1)
+        switch_places!(array,i)
       end
     end
   end
 end
 
-test = [9,2,3,0,1,8,4,1,9]
-p test
-bubble_sort(test)
-p test
+def bubble_sort(array)
+  array_copy = array.map(&:clone)
+  bubble_sort!(array_copy)
+  return array_copy
+end
